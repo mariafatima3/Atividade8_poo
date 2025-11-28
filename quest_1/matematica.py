db5214f84b2caae48851029944aa5bfda7f870f5
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-11-28 16:36:47 +0000

    quest_1

---
+++ quest_1/matematica.py
@@ -0,0 +1,11 @@
+# 1) Crie um módulo Python chamado 'matematica.py' com duas funções: 'soma(a, b)' que retorna a soma de 'a' e 'b', e 'multiplica(a, b)' que retorna a multiplicação de 'a' e 'b'. 
+# Em um arquivo separado chamado 'teste_matematica.py', importe as funções individualmente e use-as para calcular e imprimir a soma de 5 e 3, e a multiplicação de 4 e 6.
+
+def soma(a, b):
+    return a + b
+
+def multiplica(a, b):
+    return a * b
+
+    
+    
---
+++ quest_1/teste_matematica.py
@@ -0,0 +1,8 @@
+from matematica import soma, multiplica
+
+resultado_soma = soma(5, 3)
+resultado_multiplicacao = multiplica(4, 6)
+
+print(f"Resultado da soma: {resultado_soma}")
+print(f"Resultado da multiplicação: {resultado_multiplicacao}")
+
