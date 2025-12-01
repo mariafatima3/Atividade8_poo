26ae158cd918c6e0e61b6929c1bbc4a0f71bc987
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-12-01 18:58:37 +0000

    quest_4

--- quest_4/main_converter.py
+++ quest_4/main_converter.py
@@ -1,8 +1,8 @@
-from conversor_temp_1 import converter_temperatura as para_fahrenheit
-from conversor_temp_2 import converter_temperatura as para_kelvin
+from conversor_temp_1 import converter_temperatura as f
+from conversor_temp_2 import converter_temperatura as k
 
 celsius = 50
 
-print(f"{celsius}°C em Fahrenheit: {para_fahrenheit(celsius)}°F")
-print(f"{celsius}°C em Kelvin: {para_kelvin(celsius)}K")
+print(f"{celsius}°C em Fahrenheit: {f(celsius)} °F")
+print(f"{celsius}°C em Kelvin: {k(celsius)} K")
 
