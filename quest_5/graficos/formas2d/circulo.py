1e131a01d7ff8653fb9ddf9e5a849b38fd7c9d29
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-12-02 16:54:16 +0000

    quest_5

---
+++ quest_5/graficos/formas2d/circulo.py
@@ -0,0 +1,8 @@
+# 5) Crie uma estrutura de subpacotes: um pacote 'graficos' que contém um subpacote 'formas2d', e dentro de 'formas2d', um módulo 'circulo.py'. 
+# Dentro de 'circulo.py', defina uma função 'area(raio)' que calcula a área de um círculo (use 3.14 para pi). 
+# Garanta que todos os diretórios do pacote e subpacote contenham o arquivo '__init__.py'. 
+# Em um arquivo fora do pacote, chamado 'teste_area.py', importe a função 'area' do módulo 'circulo' e use-a para calcular e imprimir a área de um círculo com raio 7.
+
+def area(raio):
+    pi = 3.14
+    return pi * raio * raio
---
+++ quest_5/teste_area.py
@@ -0,0 +1,5 @@
+from graficos.formas2d.circulo import area
+
+raio = 7
+resultado = area(raio)
+print(f"A área do círculo de raio {raio} é {resultado}")
