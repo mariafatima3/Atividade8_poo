79e5152f651e24697c90ba426c2bf29cd23a62ec
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-12-02 16:08:10 +0000

    quest_4

--- quest_4/main_converter.py
+++ quest_4/main_converter.py
@@ -3,6 +3,9 @@ from conversor_temp_2 import converter_temperatura as k
 
 celsius = 50
 
+fahrenheit = f(celsius)
+kelvin = k(celsius)
+
 print(f"{celsius}°C em Fahrenheit: {f(celsius)} °F")
 print(f"{celsius}°C em Kelvin: {k(celsius)} K")
 
